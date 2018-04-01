2eb798ce9a7504d7aab48221139338a0cbb0f3ff
Yu Sun <usunyu@gmail.com>
2018-04-01 23:53:24 +0800

    Basic ionic app

---
+++ coinstat/src/pages/home/home.ts
@@ -0,0 +1,55 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { DataProvider } from '../../providers/data/data';
+import { Storage } from '@ionic/storage';
+
+@Component({
+    selector: 'page-home',
+    templateUrl: 'home.html'
+})
+export class HomePage {
+
+    objectKeys = Object.keys;
+    coins: Object;
+    likedCoins = [];
+
+    constructor(public navCtrl: NavController, private _data: DataProvider, private storage: Storage) {
+
+    }
+
+    ionViewDidLoad() {
+        
+    }
+
+    ionViewWillEnter() {
+        this.refreshCoins();
+    }
+
+    refreshCoins() {
+
+        this.storage.get('likedCoins').then((val) => {
+            // If the value is not set, then:
+            if (!val) {
+                this.likedCoins.push('BTC', 'ETH', 'IOT');
+                this.storage.set('likedCoins', this.likedCoins);
+
+                this._data.getCoins(this.likedCoins)
+                    .subscribe(res => {
+                        this.coins = res;
+                    });
+            }
+            // It's set
+            else {
+                this.likedCoins = val;
+
+                this._data.getCoins(this.likedCoins)
+                    .subscribe(res => {
+                        this.coins = res;
+                    });
+            }
+
+        });
+
+    }
+
+}
---
+++ coinstat/src/providers/data/data.ts
@@ -0,0 +1,28 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+/*
+  Generated class for the DataProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class DataProvider {
+
+    result:any;
+
+    constructor(public _http: HttpClient) {
+        console.log('Hello DataProvider Provider');
+    }
+
+    getCoins(coins) {
+        let coinlist = '';
+
+        coinlist = coins.join();
+
+        return this._http.get("https://min-api.cryptocompare.com/data/pricemulti?fsyms=" + coinlist + "&tsyms=USD");
+    
+    }
+
+}
